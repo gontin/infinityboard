b1927caab1893a1b1f8a71f466e51bdf3e04cd7a
CordeiroLucas <lcp2@cesar.school>
2025-07-24 01:07:19 -0300

    feat: Rota anotações adicionada

--- infinityboard/settings.py
+++ infinityboard/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.usuarios'
+    'apps.usuarios',
+    'apps.anotacoes'
 ]
 
 MIDDLEWARE = [
