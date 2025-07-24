28ed82a8880c2f2afda344d15508d6bcb863e2cb
CordeiroLucas <lcp2@cesar.school>
2025-07-23 23:26:49 -0300

    feat: Diretorio uploads

--- infinityboard/settings.py
+++ infinityboard/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'infinityboard.urls'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
