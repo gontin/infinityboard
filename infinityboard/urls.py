fba74abf62c23bf63ed18d519bbe793ca40860a0
CordeiroLucas <lcp2@cesar.school>
2025-07-23 23:33:05 -0300

    feat: Rota urls anotações

--- infinityboard/urls.py
+++ infinityboard/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.usuarios.urls')),
+    path('/notas', include('apps.anotacoes.urls'))
 ]
